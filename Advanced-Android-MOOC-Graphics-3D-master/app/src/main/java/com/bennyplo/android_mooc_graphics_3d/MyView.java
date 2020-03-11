20946e668aa87b5afea32f96ca2f7945b8a102a7
Lois Lene <lois.n@samsung.com>
2020-03-11 17:34:44 -0400

    draw robot: head, neck, body and legs

--- Advanced-Android-MOOC-Graphics-3D-master/app/src/main/java/com/bennyplo/android_mooc_graphics_3d/MyView.java
+++ Advanced-Android-MOOC-Graphics-3D-master/app/src/main/java/com/bennyplo/android_mooc_graphics_3d/MyView.java
@@ -10,9 +10,19 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MyView extends View {
-    private Paint redPaint; //paint object for drawing the lines
-    private Coordinate[]cube_vertices;//the vertices of a 3D cube
-    private Coordinate[]draw_cube_vertices;//the vertices for drawing a 3D cube
+    private Paint redPaint, bluePaint, pinkPaint,greenPaint;
+    private Coordinate[]cube_vertices;
+    private Coordinate[]draw_cube_vertices;
+    private Coordinate[]head_vertices;
+    private Coordinate[]upbody_vertices;
+    private Coordinate[]neck_vertices;
+    private Coordinate[]hip_vertices;
+    private Coordinate[]right_leg_vertices;
+    private Coordinate[]long_right_leg_vertices;
+    private Coordinate[]left_leg_vertices;
+    private Coordinate[]long_left_leg_vertices;
+    private Coordinate[]right_foot_vertices;
+    private Coordinate[]left_foot_vertices;
     public MyView(Context context) {
         super(context, null);
         final MyView thisview=this;
@@ -21,6 +31,22 @@ public class MyView extends View {
         redPaint.setStyle(Paint.Style.STROKE);//Stroke
         redPaint.setColor(Color.RED);
         redPaint.setStrokeWidth(2);
+
+        bluePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        bluePaint.setStyle(Paint.Style.STROKE);//Stroke
+        bluePaint.setColor(Color.BLUE);
+        bluePaint.setStrokeWidth(2);
+
+        pinkPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        pinkPaint.setStyle(Paint.Style.STROKE);//Stroke
+        pinkPaint.setColor(Color.MAGENTA);
+        pinkPaint.setStrokeWidth(2);
+
+        greenPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        greenPaint.setStyle(Paint.Style.STROKE);//Stroke
+        greenPaint.setColor(Color.GREEN);
+        greenPaint.setStrokeWidth(2);
+
         //create a 3D cube
         cube_vertices = new Coordinate[8];
         cube_vertices[0] = new Coordinate(-1, -1, -1, 1);
@@ -32,12 +58,40 @@ public class MyView extends View {
         cube_vertices[6] = new Coordinate(1, 1, -1, 1);
         cube_vertices[7] = new Coordinate(1, 1, 1, 1);
 
-        draw_cube_vertices=translate(cube_vertices,2,2,2);
-        draw_cube_vertices=scale(draw_cube_vertices,40,40,40);
+//        draw_cube_vertices=translate(cube_vertices,20,20,20);
+        head_vertices=scale(cube_vertices,100,100,50);
+        head_vertices=translate(head_vertices,500,300,50);
+
+        neck_vertices=scale(cube_vertices,50,30,50);
+        neck_vertices=translate(neck_vertices,500,430,50);
+
+        upbody_vertices=scale(cube_vertices,200,250,50);
+        upbody_vertices=translate(upbody_vertices,500,710,50);
+
+        hip_vertices=scale(cube_vertices,200,50,50);
+        hip_vertices=translate(hip_vertices,500,1010,1010);
+
+        right_leg_vertices=scale(cube_vertices,50,100,50);
+        right_leg_vertices=translate(right_leg_vertices,350,1160,50);
+
+        left_leg_vertices=scale(cube_vertices,50,100,50);
+        left_leg_vertices=translate(left_leg_vertices,650,1160,50);
+
+        long_right_leg_vertices=scale(cube_vertices,50,150,50);
+        long_right_leg_vertices=translate(long_right_leg_vertices,350,1410,50);
+
+        long_left_leg_vertices=scale(cube_vertices,50,150,50);
+        long_left_leg_vertices=translate(long_left_leg_vertices,650,1410,50);
+
+        right_foot_vertices=scale(cube_vertices,50,25,50);
+        right_foot_vertices=translate(right_foot_vertices,350,1585,50);
+
+        left_foot_vertices=scale(cube_vertices,50,25,50);
+        left_foot_vertices=translate(left_foot_vertices,650,1585,50);
 
         thisview.invalidate();//update the view
 
-        Timer timer = new Timer();
+        /*Timer timer = new Timer();
         TimerTask task = new TimerTask() {
             float position_x=0f;
             boolean dir=true;
@@ -57,7 +111,7 @@ public class MyView extends View {
                 thisview.invalidate();
             }
         };
-        timer.scheduleAtFixedRate(task,100,2);
+        timer.scheduleAtFixedRate(task,100,2);*/
 
     }
 
@@ -73,18 +127,135 @@ public class MyView extends View {
 
     private void DrawCube(Canvas canvas)
     {//draw a cube on the screen
-        DrawLinePairs(canvas, draw_cube_vertices, 0, 1, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 1, 3, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 3, 2, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 2, 0, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 4, 5, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 5, 7, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 7, 6, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 6, 4, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 0, 4, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 1, 5, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 2, 6, redPaint);
-        DrawLinePairs(canvas, draw_cube_vertices, 3, 7, redPaint);
+        DrawLinePairs(canvas, head_vertices, 0, 1, bluePaint);
+        DrawLinePairs(canvas, head_vertices, 1, 3, bluePaint);
+        DrawLinePairs(canvas, head_vertices, 3, 2, bluePaint);
+        DrawLinePairs(canvas, head_vertices, 2, 0, bluePaint);
+        DrawLinePairs(canvas, head_vertices, 4, 5, bluePaint);
+        DrawLinePairs(canvas, head_vertices, 5, 7, bluePaint);
+        DrawLinePairs(canvas, head_vertices, 7, 6, bluePaint);
+        DrawLinePairs(canvas, head_vertices, 6, 4, bluePaint);
+        DrawLinePairs(canvas, head_vertices, 0, 4, bluePaint);
+        DrawLinePairs(canvas, head_vertices, 1, 5, bluePaint);
+        DrawLinePairs(canvas, head_vertices, 2, 6, bluePaint);
+        DrawLinePairs(canvas, head_vertices, 3, 7, bluePaint);
+
+        DrawLinePairs(canvas, neck_vertices, 0, 1, pinkPaint);
+        DrawLinePairs(canvas, neck_vertices, 1, 3, pinkPaint);
+        DrawLinePairs(canvas, neck_vertices, 3, 2, pinkPaint);
+        DrawLinePairs(canvas, neck_vertices, 2, 0, pinkPaint);
+        DrawLinePairs(canvas, neck_vertices, 4, 5, pinkPaint);
+        DrawLinePairs(canvas, neck_vertices, 5, 7, pinkPaint);
+        DrawLinePairs(canvas, neck_vertices, 7, 6, pinkPaint);
+        DrawLinePairs(canvas, neck_vertices, 6, 4, pinkPaint);
+        DrawLinePairs(canvas, neck_vertices, 0, 4, pinkPaint);
+        DrawLinePairs(canvas, neck_vertices, 1, 5, pinkPaint);
+        DrawLinePairs(canvas, neck_vertices, 2, 6, pinkPaint);
+        DrawLinePairs(canvas, neck_vertices, 3, 7, pinkPaint);
+
+        DrawLinePairs(canvas, upbody_vertices, 0, 1, redPaint);
+        DrawLinePairs(canvas, upbody_vertices, 1, 3, redPaint);
+        DrawLinePairs(canvas, upbody_vertices, 3, 2, redPaint);
+        DrawLinePairs(canvas, upbody_vertices, 2, 0, redPaint);
+        DrawLinePairs(canvas, upbody_vertices, 4, 5, redPaint);
+        DrawLinePairs(canvas, upbody_vertices, 5, 7, redPaint);
+        DrawLinePairs(canvas, upbody_vertices, 7, 6, redPaint);
+        DrawLinePairs(canvas, upbody_vertices, 6, 4, redPaint);
+        DrawLinePairs(canvas, upbody_vertices, 0, 4, redPaint);
+        DrawLinePairs(canvas, upbody_vertices, 1, 5, redPaint);
+        DrawLinePairs(canvas, upbody_vertices, 2, 6, redPaint);
+        DrawLinePairs(canvas, upbody_vertices, 3, 7, redPaint);
+
+        DrawLinePairs(canvas, hip_vertices, 0, 1, pinkPaint);
+        DrawLinePairs(canvas, hip_vertices, 1, 3, pinkPaint);
+        DrawLinePairs(canvas, hip_vertices, 3, 2, pinkPaint);
+        DrawLinePairs(canvas, hip_vertices, 2, 0, pinkPaint);
+        DrawLinePairs(canvas, hip_vertices, 4, 5, pinkPaint);
+        DrawLinePairs(canvas, hip_vertices, 5, 7, pinkPaint);
+        DrawLinePairs(canvas, hip_vertices, 7, 6, pinkPaint);
+        DrawLinePairs(canvas, hip_vertices, 6, 4, pinkPaint);
+        DrawLinePairs(canvas, hip_vertices, 0, 4, pinkPaint);
+        DrawLinePairs(canvas, hip_vertices, 1, 5, pinkPaint);
+        DrawLinePairs(canvas, hip_vertices, 2, 6, pinkPaint);
+        DrawLinePairs(canvas, hip_vertices, 3, 7, pinkPaint);
+
+        DrawLinePairs(canvas, right_leg_vertices, 0, 1, bluePaint);
+        DrawLinePairs(canvas, right_leg_vertices, 1, 3, bluePaint);
+        DrawLinePairs(canvas, right_leg_vertices, 3, 2, bluePaint);
+        DrawLinePairs(canvas, right_leg_vertices, 2, 0, bluePaint);
+        DrawLinePairs(canvas, right_leg_vertices, 4, 5, bluePaint);
+        DrawLinePairs(canvas, right_leg_vertices, 5, 7, bluePaint);
+        DrawLinePairs(canvas, right_leg_vertices, 7, 6, bluePaint);
+        DrawLinePairs(canvas, right_leg_vertices, 6, 4, bluePaint);
+        DrawLinePairs(canvas, right_leg_vertices, 0, 4, bluePaint);
+        DrawLinePairs(canvas, right_leg_vertices, 1, 5, bluePaint);
+        DrawLinePairs(canvas, right_leg_vertices, 2, 6, bluePaint);
+        DrawLinePairs(canvas, right_leg_vertices, 3, 7, bluePaint);
+
+        DrawLinePairs(canvas, left_leg_vertices, 0, 1, bluePaint);
+        DrawLinePairs(canvas, left_leg_vertices, 1, 3, bluePaint);
+        DrawLinePairs(canvas, left_leg_vertices, 3, 2, bluePaint);
+        DrawLinePairs(canvas, left_leg_vertices, 2, 0, bluePaint);
+        DrawLinePairs(canvas, left_leg_vertices, 4, 5, bluePaint);
+        DrawLinePairs(canvas, left_leg_vertices, 5, 7, bluePaint);
+        DrawLinePairs(canvas, left_leg_vertices, 7, 6, bluePaint);
+        DrawLinePairs(canvas, left_leg_vertices, 6, 4, bluePaint);
+        DrawLinePairs(canvas, left_leg_vertices, 0, 4, bluePaint);
+        DrawLinePairs(canvas, left_leg_vertices, 1, 5, bluePaint);
+        DrawLinePairs(canvas, left_leg_vertices, 2, 6, bluePaint);
+        DrawLinePairs(canvas, left_leg_vertices, 3, 7, bluePaint);
+
+        DrawLinePairs(canvas, long_right_leg_vertices, 0, 1, greenPaint);
+        DrawLinePairs(canvas, long_right_leg_vertices, 1, 3, greenPaint);
+        DrawLinePairs(canvas, long_right_leg_vertices, 3, 2, greenPaint);
+        DrawLinePairs(canvas, long_right_leg_vertices, 2, 0, greenPaint);
+        DrawLinePairs(canvas, long_right_leg_vertices, 4, 5, greenPaint);
+        DrawLinePairs(canvas, long_right_leg_vertices, 5, 7, greenPaint);
+        DrawLinePairs(canvas, long_right_leg_vertices, 7, 6, greenPaint);
+        DrawLinePairs(canvas, long_right_leg_vertices, 6, 4, greenPaint);
+        DrawLinePairs(canvas, long_right_leg_vertices, 0, 4, greenPaint);
+        DrawLinePairs(canvas, long_right_leg_vertices, 1, 5, greenPaint);
+        DrawLinePairs(canvas, long_right_leg_vertices, 2, 6, greenPaint);
+        DrawLinePairs(canvas, long_right_leg_vertices, 3, 7, greenPaint);
+
+        DrawLinePairs(canvas, long_left_leg_vertices, 0, 1, greenPaint);
+        DrawLinePairs(canvas, long_left_leg_vertices, 1, 3, greenPaint);
+        DrawLinePairs(canvas, long_left_leg_vertices, 3, 2, greenPaint);
+        DrawLinePairs(canvas, long_left_leg_vertices, 2, 0, greenPaint);
+        DrawLinePairs(canvas, long_left_leg_vertices, 4, 5, greenPaint);
+        DrawLinePairs(canvas, long_left_leg_vertices, 5, 7, greenPaint);
+        DrawLinePairs(canvas, long_left_leg_vertices, 7, 6, greenPaint);
+        DrawLinePairs(canvas, long_left_leg_vertices, 6, 4, greenPaint);
+        DrawLinePairs(canvas, long_left_leg_vertices, 0, 4, greenPaint);
+        DrawLinePairs(canvas, long_left_leg_vertices, 1, 5, greenPaint);
+        DrawLinePairs(canvas, long_left_leg_vertices, 2, 6, greenPaint);
+        DrawLinePairs(canvas, long_left_leg_vertices, 3, 7, greenPaint);
+
+        DrawLinePairs(canvas, right_foot_vertices, 0, 1, redPaint);
+        DrawLinePairs(canvas, right_foot_vertices, 1, 3, redPaint);
+        DrawLinePairs(canvas, right_foot_vertices, 3, 2, redPaint);
+        DrawLinePairs(canvas, right_foot_vertices, 2, 0, redPaint);
+        DrawLinePairs(canvas, right_foot_vertices, 4, 5, redPaint);
+        DrawLinePairs(canvas, right_foot_vertices, 5, 7, redPaint);
+        DrawLinePairs(canvas, right_foot_vertices, 7, 6, redPaint);
+        DrawLinePairs(canvas, right_foot_vertices, 6, 4, redPaint);
+        DrawLinePairs(canvas, right_foot_vertices, 0, 4, redPaint);
+        DrawLinePairs(canvas, right_foot_vertices, 1, 5, redPaint);
+        DrawLinePairs(canvas, right_foot_vertices, 2, 6, redPaint);
+        DrawLinePairs(canvas, right_foot_vertices, 3, 7, redPaint);
+
+        DrawLinePairs(canvas, left_foot_vertices, 0, 1, redPaint);
+        DrawLinePairs(canvas, left_foot_vertices, 1, 3, redPaint);
+        DrawLinePairs(canvas, left_foot_vertices, 3, 2, redPaint);
+        DrawLinePairs(canvas, left_foot_vertices, 2, 0, redPaint);
+        DrawLinePairs(canvas, left_foot_vertices, 4, 5, redPaint);
+        DrawLinePairs(canvas, left_foot_vertices, 5, 7, redPaint);
+        DrawLinePairs(canvas, left_foot_vertices, 7, 6, redPaint);
+        DrawLinePairs(canvas, left_foot_vertices, 6, 4, redPaint);
+        DrawLinePairs(canvas, left_foot_vertices, 0, 4, redPaint);
+        DrawLinePairs(canvas, left_foot_vertices, 1, 5, redPaint);
+        DrawLinePairs(canvas, left_foot_vertices, 2, 6, redPaint);
+        DrawLinePairs(canvas, left_foot_vertices, 3, 7, redPaint);
     }
 
     @Override
